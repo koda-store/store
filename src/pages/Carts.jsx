f9b87205e79fe580dbedf816b4094f5a8095297a
E-Abadallah2006 <abdallaelsherbiny7@gmail.com>
2026-07-17 18:32:58 +0300

    layout

---
+++ src/pages/Carts.jsx
@@ -0,0 +1,9 @@
+function Carts() {
+  return (
+    <div>
+      <h1>Carts Page</h1>
+    </div>
+  );
+}
+
+export default Carts;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="p-8">
+      <h1>Home Page</h1>
+    </div>
+  );
+}
+
+export default Home;
